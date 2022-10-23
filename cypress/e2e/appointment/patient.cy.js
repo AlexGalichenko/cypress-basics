f97bae5a8b6df6f074acf52a230c7be28751d324
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-10-23 15:19:22 +0300

    module-3 po

--- cypress/e2e/appointment/patient.cy.js
+++ cypress/e2e/appointment/patient.cy.js
@@ -1,14 +1,17 @@
+import PatientPage from '../../support/page_object/PatientPage';
 describe('patient', () => {
     it('create new patient', () => {
-        cy.visit('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/patients');
-        cy.get('.patient-operations > .e-control').click();
-        cy.get('input[name=Name]').type('Jonh Dou');
-        cy.get('#BloodGroup > .e-float-input').click();
-        cy.get('[data-value="B+"]').click();
-        cy.get('input[name=Email]').type('johh.dou@email.com');
-        cy.get('input[name=Symptoms]').type('headache');
-        cy.get('#PatientMobile').type('1111111111');
-        cy.get('.new-patient-dialog button.e-primary').click();
-        cy.get('[aria-rowindex="7"] > .e-templatecell').should('have.text', 'Jonh Dou');
+        const patientPage = new PatientPage();
+        patientPage.open();
+        patientPage.addPatientButton.click();
+        patientPage.patientsForm.fill({
+            name: 'John Dou',
+            mobile: '1111111111',
+            email: 'john.dou@email.com',
+            symptoms: 'headache',
+            bloodGroup: 'B+'
+        });
+        patientPage.patientsForm.submitButton.click();
+        patientPage.patientRow(7).should('have.text', 'John Dou');
     });
 });
